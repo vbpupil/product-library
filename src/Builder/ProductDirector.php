7fa1155ffb35c02272e05cb7508ed492f244a6d4
Dean Haines <dean.haines@freetimers.com>
2020-10-27 12:35:03 +0000

    Added mpn, ean, product_code and barcode to physical variation

--- src/Builder/ProductDirector.php
+++ src/Builder/ProductDirector.php
@@ -113,8 +113,13 @@ class ProductDirector
                         'boxQty' => intval($v['box_qty']),
                         'reorderLevel' => intval($v['reorder_level']),
                         'minOrderQty' => intval($v['min_order_qty']),
+                        'productCode' => $v['product_code'],
+                        'barcode' => $v['barcode'],
+                        'ean' => $v['ean'],
+                        'mpn' => $v['mpn'],
                     ]
                 );
+
                 $tmpVariation->setStyleOptions($v['style_options']);
                 $tmpVariation->setPrice(
                     new SinglePrice([
