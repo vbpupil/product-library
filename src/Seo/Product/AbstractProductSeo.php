92d86bd3c0e6559c7b70c72974d6956bba6b0dff
Dean Haines <dean.haines@freetimers.com>
2021-02-02 17:32:14 +0000

    added supplier and google_shopping_custom_label_0 -4

--- src/Seo/Product/AbstractProductSeo.php
+++ src/Seo/Product/AbstractProductSeo.php
@@ -7,10 +7,18 @@ namespace vbpupil\ProductLibrary\Seo\Product;
 class AbstractProductSeo
 {
     public string $google_product_strategy;
- 
+
     public string $google_product_category = '';
 
     public string $google_product_type = '';
+
+    public string $google_product_custom_label_0 = '';
+
+    public string $google_product_custom_label_1 = '';
+
+    public string $google_product_custom_label_2 = '';
+
+    public string $google_product_custom_label_3 = '';
 
 
     /**
@@ -28,7 +36,7 @@ class AbstractProductSeo
     {
         $this->google_product_strategy = $google_product_strategy;
     }
-    
+
     /**
      * @return string
      */
@@ -61,5 +69,67 @@ class AbstractProductSeo
         $this->google_product_type = $google_product_type;
     }
 
+    /**
+     * @return string
+     */
+    public function getGoogleProductCustomLabel0(): string
+    {
+        return $this->google_product_custom_label_0;
+    }
 
+    /**
+     * @param string $google_product_custom_label_0
+     */
+    public function setGoogleProductCustomLabel0(string $google_product_custom_label_0): void
+    {
+        $this->google_product_custom_label_0 = $google_product_custom_label_0;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGoogleProductCustomLabel1(): string
+    {
+        return $this->google_product_custom_label_1;
+    }
+
+    /**
+     * @param string $google_product_custom_label_1
+     */
+    public function setGoogleProductCustomLabel1(string $google_product_custom_label_1): void
+    {
+        $this->google_product_custom_label_1 = $google_product_custom_label_1;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGoogleProductCustomLabel2(): string
+    {
+        return $this->google_product_custom_label_2;
+    }
+
+    /**
+     * @param string $google_product_custom_label_2
+     */
+    public function setGoogleProductCustomLabel2(string $google_product_custom_label_2): void
+    {
+        $this->google_product_custom_label_2 = $google_product_custom_label_2;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGoogleProductCustomLabel3(): string
+    {
+        return $this->google_product_custom_label_3;
+    }
+
+    /**
+     * @param string $google_product_custom_label_3
+     */
+    public function setGoogleProductCustomLabel3(string $google_product_custom_label_3): void
+    {
+        $this->google_product_custom_label_3 = $google_product_custom_label_3;
+    }
 }
